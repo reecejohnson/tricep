bcef8be38904b5e06cdae4179e1575aee1033cf8
Reece Johnson <reece.robert.johnson@gmail.com>
2021-03-29 13:00:19 +0100

    Apply page 🙏

--- pages/apply.js
+++ pages/apply.js
@@ -2,7 +2,7 @@ import Layout from "../src/partials/layout/layout";
 import Head from "next/head";
 import Header from "../src/partials/Header";
 import PageIllustration from "../src/partials/PageIllustration";
-import CtaContact from "../src/partials/CtaContact";
+import CtaContact from "../src/partials/Cta";
 import Footer from "../src/partials/Footer";
 
 function Apply() {
@@ -85,7 +85,7 @@ function Apply() {
                   </div>
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
-                      <button className="btn text-white bg-orange-600 hover:bg-orange-700 w-full">Send</button>
+                      <button className="btn text-white bg-teal-500 hover:bg-orange-700 w-full">Send</button>
                     </div>
                   </div>
                 </form>
